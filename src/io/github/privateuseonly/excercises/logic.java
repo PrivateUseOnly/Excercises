6630a09561d5278f1a28bedb686ee027d36e33db
Kamil Mlody <32574552+PrivateUseOnly@users.noreply.github.com>
2021-09-14 15:18:23 +0200

    Excercise 1 Convert Centigrades to Fahrenheits

---
+++ src/io/github/privateuseonly/excercises/logic.java
@@ -0,0 +1,42 @@
+package io.github.privateuseonly.excercises;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class logic {
+    private static float celsiusInput;
+    private static float fahrenheit;
+
+    public static void ConvertAll() {
+        AskForCentigradeValue();
+        GetUserInput();
+        Convert_C_To_F();
+        PrintResult();
+    }
+
+    private static void PrintResult() {
+        System.out.print(celsiusInput+" C is = "+ fahrenheit+" F");
+    }
+
+    private static float Convert_C_To_F() {
+        // F = 1.8 * Celsius Degrees + 32
+        return fahrenheit = (float) ((1.8*celsiusInput)+32);
+    }
+
+
+    private static void GetUserInput() {
+        celsiusInput = 0;
+        Scanner input = new Scanner(System.in);
+        try {
+            celsiusInput = input.nextFloat();
+            input.close();
+        }catch (InputMismatchException e){
+            AskForCentigradeValue();
+            GetUserInput();
+        }
+    }
+
+    private static void AskForCentigradeValue() {
+        System.out.print("Type the number of Celsius degrees to convert: ");
+    }
+}
---
+++ src/io/github/privateuseonly/excercises/main.java
@@ -0,0 +1,10 @@
+package io.github.privateuseonly.excercises;
+
+public class main {
+
+    public static void main(String args[]){
+    // Conversion from Celsius to Fahrenheit
+        logic.ConvertAll();
+
+    }
+}
