500f578efe7205e958a09646452c4c2a9861e619
Kamil Mlody <32574552+PrivateUseOnly@users.noreply.github.com>
2021-09-15 18:19:31 +0200

    Excercise 2 put 3 integers and show only the lowest and highest numbers from them

---
+++ src/io/github/privatteuseonly/excercises/excercise2/printMinMaxValue.java
@@ -0,0 +1,14 @@
+package io.github.privatteuseonly.excercises.excercise2;
+
+public class printMinMaxValue {
+// Get from user 3 integers &
+// print only lowest and highest value integers
+
+
+    public static void main(String arg[]){
+
+        calculations.showResult();
+
+    }
+
+}
