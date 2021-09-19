8357bc4b21a307445b8f4ca6fcaae35eb129008c
Kamil Mlody <32574552+PrivateUseOnly@users.noreply.github.com>
2021-09-19 22:42:25 +0200

    Refactored Example 1 = Classes  CelsiusToFahrenheit and Logic

--- src/io/github/privateuseonly/excercises/ex1/convertCelsiustoFahrenheit/CentigradesToFahrenheit.java
+++ src/io/github/privateuseonly/excercises/ex1/convertCelsiustoFahrenheit/CentigradesToFahrenheit.java
@@ -2,6 +2,7 @@ package io.github.privateuseonly.excercises.ex1.convertCelsiustoFahrenheit;
 
 public class CentigradesToFahrenheit {
 
+
     public static void main(String[] args){
     // Conversion from Celsius to Fahrenheit
         Logic.ConvertAll();
--- src/io/github/privateuseonly/excercises/ex1/convertCelsiustoFahrenheit/Logic.java
+++ src/io/github/privateuseonly/excercises/ex1/convertCelsiustoFahrenheit/Logic.java
@@ -5,22 +5,21 @@ import java.util.Scanner;
 
 public class Logic {
     private static float celsiusInput;
-    private static float fahrenheit;
 
     public static void ConvertAll() {
         AskForCentigradeValue();
         GetUserInput();
-        Convert_C_To_F();
         PrintResult();
     }
 
-    private static void PrintResult() {
-        System.out.print(celsiusInput+" C is = "+ fahrenheit+" F");
+    private static void PrintResult()
+    {
+        System.out.print(celsiusInput+" C is = "+ Convert_C_To_F()+" F");
     }
 
     private static float Convert_C_To_F() {
         // F = 1.8 * Celsius Degrees + 32
-        return fahrenheit = (float) ((1.8*celsiusInput)+32);
+        return (float) ((1.8 * celsiusInput) + 32);
     }
 
 
